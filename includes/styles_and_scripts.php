26218ec215c52cc3f9c317e12281ee4e1894c30e
Prospect Ogujiuba <priz@Prospects-MacBook-Pro.local>
2024-07-29 21:08:38 -0400

    fixed styles and scripts

--- includes/styles_and_scripts.php
+++ includes/styles_and_scripts.php
@@ -6,29 +6,18 @@
  * @package makerstarter
  */
 
+// Global variables
+$plugin_path = plugin_dir_path(__FILE__);
+$script_version = filemtime($plugin_path . 'assets/js/index.js');
+$style_version = filemtime($plugin_path . 'assets/css/styles.css');
+
 // Enqueue styles and scripts
 function makerstarter_enqueue()
 {
-  $theme_version = filemtime(get_stylesheet_directory() . '/style.css');
-  $script_version = filemtime(get_stylesheet_directory() . '/assets/js/index.js');
-  $style_version = filemtime(get_stylesheet_directory() . '/assets/css/styles.css');
-
-  /* Register and Enqueue Admin Scripts */
-
-  // wp_register_script('makerstarter-admin-scripts', get_parent_theme_file_uri('/assets/js/index-wp-admin.js'), [], $script_version, true);
-  // wp_enqueue_script('makerstarter-admin-scripts');
-
-  /* Register and Enqueue Admin Styles */
-
-  wp_register_style('makerstarter-admin-styles', get_parent_theme_file_uri('/assets/css/styles-wp-admin.css'), [], $style_version, 'all');
-  wp_enqueue_style('makerstarter-admin-styles');
-
-  /* Register and Enqueue Front End Scripts */
+  global $script_version, $style_version;
 
   wp_register_script('makerstarter-scripts', get_parent_theme_file_uri('/assets/js/index.js'), ['wp-element'], $script_version, true);
   wp_enqueue_script('makerstarter-scripts');
-
-  /* Register and Enqueue Front End Styles */
 
   wp_register_style('makerstarter-styles', get_parent_theme_file_uri('/assets/css/styles.css'), [], $style_version, 'all');
   wp_enqueue_style('makerstarter-styles');
@@ -36,3 +25,16 @@ function makerstarter_enqueue()
 
 // Enqueue front-end styles and script
 add_action('wp_enqueue_scripts', 'makerstarter_enqueue');
+
+function makerstarter_admin_enqueue()
+{
+  global $plugin_directory, $script_version, $style_version;
+
+  // wp_register_script('makerstarter-admin-scripts', get_parent_theme_file_uri('/assets/js/index-wp-admin.js'), [], $script_version, true);
+  // wp_enqueue_script('makerstarter-admin-scripts');
+
+  wp_register_style('makerstarter-admin-styles', get_parent_theme_file_uri('/assets/css/styles-wp-admin.css'), [], $style_version, 'all');
+  wp_enqueue_style('makerstarter-admin-styles');
+}
+
+add_action('admin_enqueue_scripts', 'makerstarter_admin_enqueue');
